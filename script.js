a03e011f563a3db5698d0e9280472314c006a4f4
eimeitakagi <eimei0419@gmail.com>
2015-12-03 16:13:42 +0900

    画面内リンクのスクロールを実装

--- script.js
+++ script.js
@@ -3,9 +3,7 @@ $(function(){
   var status = "close";
 
   $('#read-more').click(function(){
-
     $('#text-js').fadeToggle();
-
     if (text == "close"){
       $(this).text("閉じる");
       status = "open";
@@ -13,7 +11,16 @@ $(function(){
       $(this).text("もっと見る");
       status = "close";
     }
-
   });
+
+  $('a[href^=#]').click(function(){
+    var speed = 700;
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $("html, body").animate({scrollTop:position}, speed, "swing");
+    return false;
+  });
+
 
 });
