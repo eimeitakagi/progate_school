4e54de77d6a1e7fcad740a0e5a0ebc132e526528
eimeitakagi <eimei0419@gmail.com>
2015-12-03 15:51:22 +0900

    scriptファイル追加、もっと見るを実装

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 $(function(){
 
+  var status = "close";
+
+  $('#read-more').click(function(){
+
+    $('#text-js').fadeToggle();
+
+    if (text == "close"){
+      $(this).text("閉じる");
+      status = "open";
+    }else{
+      $(this).text("もっと見る");
+      status = "close";
+    }
+
+  });
 
 });
