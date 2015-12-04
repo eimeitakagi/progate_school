7fbfb106de263eb7fd15486f1db128472ce67034
eimeitakagi <eimei0419@gmail.com>
2015-12-05 02:02:11 +0900

    bxslider追加、要修正

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 $(function(){
 
+//もっと詳しく
   var status = "close";
   $('#read-more').click(function(){
     $('#text-js').fadeToggle();
@@ -12,6 +13,7 @@ $(function(){
     }
   });
 
+//ページ内リンクのスクロール
   $('a[href^=#]').click(function(){
     var speed = 700;
     var href= $(this).attr("href");
@@ -19,6 +21,18 @@ $(function(){
     var position = target.offset().top;
     $("html, body").animate({scrollTop:position}, speed, "swing");
     return false;
+  });
+
+//スライド
+  $('#bxSlider').bxSlider({
+    auto: 'true',
+    pause: 5000,
+    speed: 700,
+    infiniteLoop: 'true',
+    pager: 'true',
+    controls: 'true',
+    nextText: '',
+    prevText:''
   });
 
 
